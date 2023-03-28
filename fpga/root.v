4883545076f42a967c85aba17e2cd8de277f938b
Mustafa Miyaziwala <mmiyazi@umich.edu>
2023-03-27 22:14:27 -0400

    Update root

--- fpga/root.v
+++ fpga/root.v
@@ -1,20 +1,25 @@
-module root(input sys_clk, input sys_rst_n, output trig_0, output echo_0);
+module root(input sys_clk, input sys_rst_n, output trig_0, input echo_0, output test_0);
 
-    reg [4:0] ultra_clk;
+    reg [3:0] counter;
+    reg ultra_clk;
 
-    always @(posedge sys_clk or negedge sys_rst_n) begin
-        if (!sys_rst_n)
-            ultra_clk <= 5'd0;
-        else if (counter < 5'd24) // 1 us clock period
-            ultra_clk <= ultra_clk + 1;
-        else
-            ultra_clk <= 5'd0;
+    initial begin
+        ultra_clk <= 0;
+    end
+
+    always @(posedge sys_clk ) begin
+        if (counter < 5'd11) // 1 us clock period
+            counter <= counter + 1;
+        else begin
+            counter <= 5'd0;
+            ultra_clk <= ~ultra_clk;
+        end
     end
 
 
-    reg [5:0] pulse_0;
+    ultrasonic ultra_0(trig_0, echo_0, sys_rst_n, ultra_clk, test_0);
 
-    ultrasonic ultra_0(pulse_0, trig_0, echo_0, sys_rst_n, ultra_clk);
+    //assign trig_0 = echo_0;
 
 
-endmodule
+endmodule  
